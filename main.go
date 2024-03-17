6751a1ae20e8debfe9e16f6f56f25ef67098c2cd
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2024-03-17 16:43:57 +0100

    frankfurter response handler

--- main.go
+++ main.go
@@ -1,12 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
+
+type FrankfurterResponse struct {
+	Amount    float64                       `json:"amount"`
+	Base      string                        `json:"base"`
+	StartDate string                        `json:"start_date"`
+	EndDate   string                        `json:"end_date"`
+	Rates     map[string]map[string]float64 `json:"rates"`
+}
+
+func (fr *FrankfurterResponse) get(w http.ResponseWriter, s string, e string, b string, t string) error {
+	var date string
+	if s == e {
+		date = s
+	} else {
+		date = fmt.Sprintf("%s..%s", s, e)
+	}
+
+	reqURL := fmt.Sprintf("https://api.frankfurter.app/%s?from=%s&to=%s", date, b, t)
+	resp, err := http.Get(reqURL)
+
+	if err != nil {
+		errMsg := fmt.Sprintf("Error fetching from %s: %v", reqURL, err)
+		http.Error(w, errMsg, resp.StatusCode)
+		return err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(fr)
+	if err != nil {
+		errMsg := fmt.Sprintf("Server errored while processing data from %s", reqURL)
+		http.Error(w, errMsg, http.StatusInternalServerError)
+		return err
+	}
+
+	return nil
+}
 
 func neededFormFields() []string {
 	return []string{"base-curr", "target-curr", "start-date", "end-date"}
@@ -29,8 +65,18 @@ func getResource(w http.ResponseWriter, r *http.Request) {
 			params[field] = p
 		}
 	}
+	s := params["start-date"]
+	e := params["end-date"]
+	b := params["base-curr"]
+	t := params["target-curr"]
 
-	fmt.Println(params)
+	frResp := new(FrankfurterResponse)
+	err := frResp.get(w, s, e, b, t)
+	if err != nil {
+		return
+	}
+
+	fmt.Printf("%+v\n", *frResp)
 }
 
 func main() {
